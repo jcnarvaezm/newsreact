b0b7640a31688c1c8508b9ff9f5c7a1fddc6d2ab
jcnarvaezm <jnarvaez@condorlabs.io>
2020-06-03 08:43:56 -0500

    add useFetch test

--- src/components/notice/notice.test.js
+++ src/components/notice/notice.test.js
@@ -1,18 +1,3 @@
-// import React from 'react';
-// import { configure, shallow } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-// import Notice from './notice';
-
-// configure({
-//   adapter: new Adapter(),
-// });
-
-// describe('Testing Notice Component', () => {
-//   test('Testing rendering of Notice Component', () => {
-//     const wrapper = shallow(<Notice />);
-//     expect(wrapper.find('section').exists()).toBe(true);
-//   });
-// });
 import React from 'react';
 import { create, act } from 'react-test-renderer';
 import { configure } from 'enzyme';
---
+++ src/controllers/useFetch.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import NewsContainer from '../components/newsContainer/newsContainer';
+
+configure({
+  adapter: new Adapter(),
+});
+
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: jest.fn(),
+}));
+
+describe('Testing Form Container', () => {
+  let wrapper;
+  const setDataNews = jest.fn();
+  const useStateSpy = jest.spyOn(React, 'useState');
+
+  const useStateMock: any = (initState: any) => [initState, setDataNews];
+
+  useStateSpy.mockImplementation((dataNews) => [dataNews, setDataNews]);
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('calls setDataNews with 1', () => {
+    jest.spyOn(React, 'useState').mockImplementation(useStateMock);
+    wrapper = mount(<NewsContainer count={1} />);
+    expect(setDataNews).toHaveBeenCalledTimes(1);
+  });
+});
